3a05740784e1684209f703f6066db6e163edf57a
SaiChandra2896 <saichandra2896@gmail.com>
2019-12-03 12:58:26 +0530

    add test route to register user

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => res.send('User route'));
+//register user
+router.post('/', (req, res) => {
+    console.log(req.body);
+    res.send('user route')
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const connectDB = require('./config/db');
 
+const app = express();
 //connect DB
 connectDB();
 
-const app = express();
+//init middleware
+app.use(express.json({ extended: false }))
 
 app.get('/', (req, res) => res.send('API Running'));
 
