092a3de586780c6e9239d4bf9c193607e470df66
SaiChandra2896 <saichandra2896@gmail.com>
2019-12-10 14:17:03 +0530

    add route to get post by id

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -34,6 +34,35 @@ router.post('/', [auth, [
         res.status(500).send('Server Error');
     }
 
-})
+});
+
+//get all posts
+router.get('/', auth, async (req, res) => {
+    try {
+        const posts = await Post.find().sort({ date: -1 });
+        res.json(posts);
+    } catch (err) {
+        console.log(err.message);
+        res.status(500).send('Server Error');
+    }
+});
+
+//get single post by id
+router.get('/:id', auth, async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+
+        if (!post) {
+            return res.status(404).json({ msg: 'Post not found' });
+        }
+        res.json(post);
+    } catch (err) {
+        console.log(err.message);
+        if (err.kind === 'ObjectId') {
+            return res.status(404).json({ msg: 'Post not found' });
+        }
+        res.status(500).send('Server Error');
+    }
+});
 
 module.exports = router;
