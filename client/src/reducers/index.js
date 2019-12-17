c8be93007fbb372eca0bfbcd70cc6deb4e126a81
SaiChandra2896 <saichandra2896@gmail.com>
2019-12-17 14:32:08 +0530

    add alert reducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,4 +1,7 @@
 //this is the rootreducer
 import { combineReducers } from 'redux';
+import alert from './alert';
 
-export default combineReducers({});
+export default combineReducers({
+    alert
+});
