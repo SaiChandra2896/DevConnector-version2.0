5a7930c46c82619ca4d856526209d84cea06ba86
SaiChandra2896 <saichandra2896@gmail.com>
2019-12-20 13:48:38 +0530

    add profileTop component to display dp and about

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -3,19 +3,23 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
 import { getProfilebyId } from '../../actions/profile';
+import ProfileTop from './ProfileTop';
 import { Link } from 'react-router-dom';
 
 const Profile = ({ getProfilebyId, profile: { profile, loading }, auth, match }) => {
     useEffect(() => {
         getProfilebyId(match.params.id);
-    }, [getProfilebyId]);
+    }, [getProfilebyId, match.params.id]);
     return (
         <Fragment>
             {profile === null || loading ? <Spinner /> : (<div>
                 <Link to="/profiles" className="btn btn-light">Back to Profiles</Link>
                 {auth.isAuthenticated && auth.loading === false && auth.user._id === profile.user._id && (
-                    <Link to="edit-profile" className="btn btn-dark"> Edit Profile
+                    <Link to="/edit-profile" className="btn btn-dark"> Edit Profile
                 </Link>)}
+                <div class="profile-grid my-1">
+                    <ProfileTop profile={profile} />
+                </div>
             </div>)}
         </Fragment>
     );
